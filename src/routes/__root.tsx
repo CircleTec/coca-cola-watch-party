a2bfe0b7c25d42827a1c1dc995c152c87a59a54b
natedamtew <nathandamtew@gmail.com>
2026-09-09 13:15:29 +0300

    chore: update favicon to use Fanta logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -114,7 +114,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: GOOGLE_FONTS_URL,
       },
-      { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "icon", href: "/fanta-logo-white-outline.png", type: "image/png" },
     ],
   }),
   shellComponent: RootShell,
